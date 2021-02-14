eeb73119e26419ec19a36d6b6d1bb2c88db71cae
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-14 10:32:39 +0530

    [vijay]first commit

---
+++ MoodAnalyserDay12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserDay12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
